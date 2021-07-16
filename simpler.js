efb99df1ccdcf07a8260eac9a661ba01bb171701
jcarlosvegadiaz <87079770+jcarlosvegadiaz@users.noreply.github.com>
2021-07-16 12:46:24 -0400

    Cambio de escala de zoom

--- simpler.js
+++ simpler.js
@@ -21,7 +21,7 @@ $(window).load(function() {
         console.log(backgroundSizeX)
         console.log("THE TYPE is " + typeof(backgroundSizeX))
         console.log(backgroundSizeY)
-        if (backgroundSizeX > 20000) {
+        if (backgroundSizeX > 90000) {
           window.location = './page3.html'
         }
     });
