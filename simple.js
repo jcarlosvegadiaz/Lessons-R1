bb06a06942fc7e8fb6ad99adc55947fb675e2ed8
jcarlosvegadiaz <87079770+jcarlosvegadiaz@users.noreply.github.com>
2021-07-19 18:55:09 -0400

    If img arch, img people then page 2 or page 3

--- simple.js
+++ simple.js
@@ -1,32 +1,33 @@
 var images = [
-    'backgroundimages/00.JPG',
-    'backgroundimages/01.JPG',
-    'backgroundimages/02.JPG',
-    'backgroundimages/03.JPG',
-    'backgroundimages/04.JPG',
-    'backgroundimages/05.JPG',
-    'backgroundimages/06.JPG',
-    'backgroundimages/07.JPG',
-    'backgroundimages/08.JPG',
-    'backgroundimages/09.JPG',
-    'backgroundimages/10.JPG',
-    'backgroundimages/11.JPG',
-    'backgroundimages/12.JPG',
-    'backgroundimages/13.JPG',
-    'backgroundimages/14.JPG',
-    'backgroundimages/15.JPG',
-    'backgroundimages/16.JPG',
-    'backgroundimages/17.JPG',
-    'backgroundimages/18.JPG',
-    'backgroundimages/19.JPG'
+  {img:'backgroundimages/00.JPG',  link: './page2.html'},
+  {img:'backgroundimages/01.JPG',  link: './page2.html'},
+  {img:'backgroundimages/02.JPG',  link: './page2.html'},
+  {img:'backgroundimages/03.JPG',  link: './page2.html'},
+  {img:'backgroundimages/04.JPG',  link: './page2.html'},
+  {img:'backgroundimages/05.JPG',  link: './page2.html'},
+  {img:'backgroundimages/06.JPG',  link: './page2.html'},
+  {img:'backgroundimages/07.JPG',  link: './page2.html'},
+  {img:'backgroundimages/08.JPG',  link: './page2.html'},
+  {img:'backgroundimages/09.JPG',  link: './page2.html'},
+  {img:'backgroundimages/10.JPG',  link: './page3.html'},
+  {img:'backgroundimages/11.JPG',  link: './page3.html'},
+  {img:'backgroundimages/12.JPG',  link: './page3.html'},
+  {img:'backgroundimages/13.JPG',  link: './page3.html'},
+  {img:'backgroundimages/14.JPG',  link: './page3.html'},
+  {img:'backgroundimages/15.JPG',  link: './page3.html'},
+  {img:'backgroundimages/16.JPG',  link: './page3.html'},
+  {img:'backgroundimages/17.JPG',  link: './page3.html'},
+  {img:'backgroundimages/18.JPG',  link: './page3.html'},
+  {img:'backgroundimages/19.JPG',  link: './page3.html'}
   ];
 
 $(window).load(function() {
   var num = Math.floor(Math.random() * 20);
 
-  var img = $('<img id="fotos" src="' + images[num] + '" style="width:100%">');
+  var img = $('<img id="fotos" src="' + images[num].img + '" style="width:100%">');
   img.appendTo('#container');
   wheelzoom(img);
+  var link=images[num].link;
   // to go to page2 just set the window.location
   // window.location = 'page2.html'
   // you'll want to listen to the change events on the img for when the background-size changes
@@ -42,7 +43,7 @@ $(window).load(function() {
         console.log("THE TYPE is " + typeof(backgroundSizeX))
         console.log(backgroundSizeY)
         if (backgroundSizeX > 30000) {
-          window.location = './page2.html'
+          window.location = link;
         }
     });
   });
