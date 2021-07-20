ec142ad3b37ee89928f19f6cc2992761d6e462db
jcarlosvegadiaz <87079770+jcarlosvegadiaz@users.noreply.github.com>
2021-07-20 18:30:35 -0400

    Revision de definicion

--- simpler.js
+++ simpler.js
@@ -1,12 +1,13 @@
 var images = [
-  'images/11 nformation.png'
-];
+  '/graphic/11 nformation.png'];
 
 $(window).load(function() {
-  var num = Math.floor(Math.random() * 1);
-  var img = $('<img id="fotos" src="' + images[num] + '" style="width:100%">');
-  img.appendTo('#nformation');
+  var num = [0];
+
+  var img = $('<img id="fotos" src="' + images[num].img + '" style="width:100%">');
+  img.appendTo('#screen');
   wheelzoom(img);
+  // var link=images[num].link;
   // to go to page2 just set the window.location
   // window.location = 'page2.html'
   // you'll want to listen to the change events on the img for when the background-size changes
@@ -21,12 +22,12 @@ $(window).load(function() {
         console.log(backgroundSizeX)
         console.log("THE TYPE is " + typeof(backgroundSizeX))
         console.log(backgroundSizeY)
-        if (backgroundSizeX > 90000) {
-          window.location = './page3.html'
-        }
+        // if (backgroundSizeX > 30000) {
+        //   window.location = link;
+        // }
     });
   });
 
   var domImg = document.getElementById('fotos')
   observer.observe(domImg, { attributes : true, attributeFilter : ['style'] });
-// });
+});
